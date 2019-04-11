6f8f323e1044698f01b4adb6118d0605b7beff27
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-04-10 21:49:45 -0400

    test

--- assets/script/firebase.js
+++ assets/script/firebase.js
@@ -57,3 +57,34 @@ var uiConfig = {
     // Privacy policy url.
     privacyPolicyUrl: '<your-privacy-policy-url>'
   };
+
+
+
+
+
+// CHAT
+  function login() {
+    // Log the user in via Twitter
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).catch(function(error) {
+      console.log("Error authenticating user:", error);
+    });
+  }
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    // Once authenticated, instantiate Firechat with the logged in user
+    if (user) {
+      initChat(user);
+    }
+  });
+
+  function initChat(user) {
+    // Get a Firebase Database ref
+    var chatRef = firebase.database().ref("chat");
+
+    // Create a Firechat instance
+    var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
+
+    // Set the Firechat user
+    chat.setUser(user.uid, user.displayName);
+  }
