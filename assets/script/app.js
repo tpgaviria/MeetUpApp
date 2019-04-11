6afa8944df30e08ce9dd42299eaa3eb1ff6ac9cc
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-04-10 21:20:14 -0400

    test all

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,20 +1,65 @@
 console.log('testing, hi this is the correct js file :)')
 
 
-$(document).ready(function() {
+// materialize - text input styling initialize
+$(document).ready(function () {
   $('input#input_text, textarea#textarea2').characterCounter();
 });
-      
 
-// Or with jQuery
 
-$(document).ready(function(){
+
+// materialize - modal functionality
+$(document).ready(function () {
   $('.modal').modal();
 });
-      
 
 
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-        
+// materialize - small screen sidebar nav
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
+
+
+
+
+
+
+// map search
+
+$('button').on('click', function () {
+
+  event.preventDefault();
+  addressSearch();
+})
+
+function addressSearch() {
+
+  // placeSearch({
+  //   key: 'SzMAPmTeOI5jHoAV1AdN1Ro2g1r8lACM',
+  //   container: document.querySelector('.place-search-input'),
+  //   useDeviceLocation: true
+  // });
+
+  var geocodingKey = 'SzMAPmTeOI5jHoAV1AdN1Ro2g1r8lACM'
+  var location1 = $('#location1').val();
+  var location2 = $('#location2').val();
+  var geocodingURL = 'https://www.mapquestapi.com/geocoding/v1/batch?&inFormat=kvp&outFormat=json&thumbMaps=false&maxResults=1&location=' + location1 + '&location=' + location2 + '&key=' + geocodingKey;
+
+
+  console.log(location1);
+  console.log(location2);
+  console.log(geocodingURL);
+
+
+  $.ajax({
+    url: geocodingURL
+  }).done(function (response) {
+    console.log(response);
+  })
+}
+
+
+
+function middlePoint() {
+
+}
