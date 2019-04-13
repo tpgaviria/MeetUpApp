c8c603282833b82133bfbe9de8ce53c7faf251c4
Claudia Gutierrez <gutierrez.cp21@yahoo.com>
2019-04-13 10:45:39 -0400

    content

--- assets/script/app.js
+++ assets/script/app.js
@@ -69,23 +69,23 @@ function addressSearch() {
     var loc1lat = loc1results.latLng.lat;
     var loc1lng = loc1results.latLng.lng;
     var loc1coords = loc1lat + ', ' + loc1lng;
-    
+
     console.log(loc1lat);
 
 
     var loc2lat = loc2results.latLng.lat;
     var loc2lng = loc2results.latLng.lng;
     var loc2coords = loc2lat + ', ' + loc2lng;
-    
+
     console.log('location type: ' + loc1type + ', ' + loc1coords);
     console.log(loc2coords);
 
-    
+
   })
 
 
-$('.side-panel').append(location1 + location2);
-$('.side-panel').append('<p>' + results + '</p>');
+  $('.side-panel').append(location1 + location2);
+  $('.side-panel').append('<p>' + results + '</p>');
 
 
 }
@@ -95,7 +95,43 @@ $('.side-panel').append('<p>' + results + '</p>');
 function middlePoint() {
 
 
+}
 
+function displayPlaces() {
 
+  var places = $(this).attr("data-name");
+  var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?&location=atlanta";
+
+  // Creating an AJAX call for the specific movie button being clicked
+  $.ajax({
+    url: queryURL,
+    headers: { 'Authorization': 'Bearer cTXs93Tu7cOPhOYuXLLZdE5SIIkZRBS19EXdpPFQ3kBP7QyfYF3Uwbk6ZzwygDmXzdFKv0g8ndmZecAPAdKKOm3aeqFhD_wrH2DP6vmneVo0nRIO90SbPc-hjZKuXHYx' },
+    method: "GET",
+    dataType: 'json',
+  })
+    .then(function (response) {
+      console.log(response);
+      console.log("hi this is me");
+    
+
+      var name = response.name;
+      var pOne = $("<h3></h3>").text(name);
+      side - panel.append(pOne);
+      var address = response.Address;
+      var pTwo = $("<p>").text(address);
+      side - panel.append(pTwo);
+      var categories = response.Categories;
+      var pThree = $("<p>").text(categories);
+      side - panel.append(pThree);
+      var reviews = response.Reviews;
+      var pFour =  $("<p>").text(reviews);
+      side-panel.append(pFour);
+      var imgURL = response.Image;
+      var image = $("<img>").attr("src", imgURL);
+      side - panel.append(image);
+
+      $("#places-view").prepend(side - panel);
+    });
 
 }
+displayPlaces();
