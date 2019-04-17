9b8fc0a4f18eda2a41db1c06786d378bcad029e5
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-04-17 18:37:21 -0400

    auth

--- assets/script/firebase.js
+++ assets/script/firebase.js
@@ -59,17 +59,25 @@ var uiConfig = {
 
 
 
-  var user = firebase.auth().currentUser;
+  
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      var user = firebase.auth().currentUser;
+      if (user != null) {
+        user.providerData.forEach(function (profile) {
+          console.log("Sign-in provider: " + profile.providerId);
+          console.log("  Provider-specific UID: " + profile.uid);
+          console.log("  Name: " + profile.displayName);
+          console.log("  Email: " + profile.email);
+          console.log("  Photo URL: " + profile.photoURL);
+        });
+      }
+      // User is signed in.
+    } else {
+      // No user is signed in.
+    }
+  });
 
-  if (user != null) {
-    user.providerData.forEach(function (profile) {
-      console.log("Sign-in provider: " + profile.providerId);
-      console.log("  Provider-specific UID: " + profile.uid);
-      console.log("  Name: " + profile.displayName);
-      console.log("  Email: " + profile.email);
-      console.log("  Photo URL: " + profile.photoURL);
-    });
-  }
 
 
   // firebase.auth().onAuthStateChanged(function(user) {
