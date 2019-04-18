686b7cf5f5f32db32475553972f0cbd044be5606
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-04-18 19:29:35 -0400

    auth

--- assets/script/firebase.js
+++ assets/script/firebase.js
@@ -44,51 +44,6 @@ ui.start('#firebaseui-auth-container', uiConfig);
 
 
 
-initApp = function () {
-  firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-      // User is signed in.
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var uid = user.uid;
-      var phoneNumber = user.phoneNumber;
-      var providerData = user.providerData;
-      user.getIdToken().then(function (accessToken) {
-        document.getElementById('sign-in-status').textContent = 'Signed in';
-        document.getElementById('sign-in').textContent = 'Sign out';
-        document.getElementById('account-details').textContent = JSON.stringify({
-          displayName: displayName,
-          email: email,
-          emailVerified: emailVerified,
-          phoneNumber: phoneNumber,
-          photoURL: photoURL,
-          uid: uid,
-          accessToken: accessToken,
-          providerData: providerData
-        }, null, '  ');
-      });
-    } else {
-      // User is signed out.
-      document.getElementById('sign-in-status').textContent = 'Signed out';
-      document.getElementById('sign-in').textContent = 'Sign in';
-      document.getElementById('account-details').textContent = 'null';
-    }
-  }, function (error) {
-    console.log(error);
-  });
-};
-
-window.addEventListener('load', function () {
-  initApp()
-});
-
-
-
-
-
-
 // if user signed in, change nav bar
 var user = firebase.auth().currentUser;
 
@@ -111,8 +66,8 @@ firebase.auth().onAuthStateChanged(function (user) {
 
 
       $('#greeting').text('Hey, ' + name + '!');
-      $('#no-user').hide();
       $('#signed-in').show();
+      $('#no-user').hide();
 
 
       $('#sign-out').on('click', function () {
