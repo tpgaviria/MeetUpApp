b74ac801276f89210531820bac6f9818e997f21f
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-04-18 09:53:06 -0400

    auth

--- assets/script/firebase.js
+++ assets/script/firebase.js
@@ -11,77 +11,20 @@ var config = {
 };
 firebase.initializeApp(config);
 
+
+
+
+
+
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-
-ui.start('#firebaseui-auth-container', uiConfig);
-
-ui.start('#firebaseui-auth-container', {
-  signInOptions: [
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-  ]
-  // Other config options...
-});
-
-
-
-
 
 
 var uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult: function (authResult, redirectUrl) {
 
-      firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-          var user = firebase.auth().currentUser;
-          var name;
-          if (user) {
-            user.providerData.forEach(function (profile) {
-              console.log("Sign-in provider: " + profile.providerId);
-              console.log("  Provider-specific UID: " + profile.uid);
-              console.log("  Name: " + profile.displayName);
-              console.log("  Email: " + profile.email);
-              console.log("  Photo URL: " + profile.photoURL);
 
-              name = profile.displayName;
-              profile = profile;
-              console.log(profile);
 
-            });
-
-          }
-
-          
-          
-          
-          $('#sign-out').on('click', function () {
-            
-            firebase.auth().signOut().then(function () {
-              
-              $('#no-user').show();
-              $('#signed-in').hide();
-              
-              console.log('signed out succesfully');
-              // Sign-out successful.
-            }).catch(function (error) {
-              // An error happened.
-              console.log('error signing out');
-              
-            })
-          })
-          
-          
-          
-          // User is signed in.
-        } else {
-          // No user is signed in.
-          
-          console.log('no user signed in');
-        }
-      });
-      
       // User successfully signed in.
       // Return type determines whether we continue the redirect automatically
       // or whether we leave that to developer to handle.
@@ -95,7 +38,7 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
-  signInSuccessUrl: '<url-to-redirect-to-on-success>',
+  signInSuccessUrl: 'https://tpgaviria.github.io/MeetUpApp/',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -109,6 +52,13 @@ var uiConfig = {
 
 
 
+ui.start('#firebaseui-auth-container', uiConfig);
+
+
+
+
+
+
 
 var user = firebase.auth().currentUser;
 
@@ -116,6 +66,60 @@ if (user) {
   $('#greeting').text('Hey, ' + name + '!');
   $('#no-user').hide();
   $('#signed-in').show();
+
+
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      var user = firebase.auth().currentUser;
+      var name;
+      if (user) {
+        user.providerData.forEach(function (profile) {
+          console.log("Sign-in provider: " + profile.providerId);
+          console.log("  Provider-specific UID: " + profile.uid);
+          console.log("  Name: " + profile.displayName);
+          console.log("  Email: " + profile.email);
+          console.log("  Photo URL: " + profile.photoURL);
+
+          name = profile.displayName;
+          profile = profile;
+          console.log(profile);
+
+        });
+
+      }
+
+
+
+
+      $('#sign-out').on('click', function () {
+
+        firebase.auth().signOut().then(function () {
+
+          $('#no-user').show();
+          $('#signed-in').hide();
+
+          console.log('signed out succesfully');
+          // Sign-out successful.
+        }).catch(function (error) {
+          // An error happened.
+          console.log('error signing out');
+
+        })
+      })
+
+
+
+      // User is signed in.
+    } else {
+      // No user is signed in.
+
+      console.log('no user signed in');
+    }
+  });
+
+
+
   // User is signed in.
 } else {
   // No user is signed in.
@@ -125,101 +129,153 @@ if (user) {
 
 
 
-// firebase.auth().onAuthStateChanged(function (user) {
-  //   if (user) {
-//     var user = firebase.auth().currentUser;
-//     var name;
-//     if (user != null) {
-  //       user.providerData.forEach(function (profile) {
+
+
+
+
+
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    var user = firebase.auth().currentUser;
+    name = null;
+    if (user) {
+      user.providerData.forEach(function (profile) {
+        console.log("Sign-in provider: " + profile.providerId);
+        console.log("  Provider-specific UID: " + profile.uid);
+        console.log("  Name: " + profile.displayName);
+        console.log("  Email: " + profile.email);
+        console.log("  Photo URL: " + profile.photoURL);
+
+        name = profile.displayName;
+        profile = profile;
+        console.log(profile);
+
+
+        $('#sign-out').on('click', function () {
+
+          firebase.auth().signOut().then(function () {
+
+            $('#no-user').show();
+            $('#signed-in').hide();
+
+            console.log('signed out succesfully');
+            // Sign-out successful.
+          }).catch(function (error) {
+            // An error happened.
+            console.log('error signing out');
+
+          })
+        })
+
+
+
+      });
+
+    }
+
+
+
+
+
+
+
+
+    // firebase.auth().onAuthStateChanged(function (user) {
+    //   if (user) {
+    //     var user = firebase.auth().currentUser;
+    //     var name;
+    //     if (user != null) {
+    //       user.providerData.forEach(function (profile) {
     //         console.log("Sign-in provider: " + profile.providerId);
     //         console.log("  Provider-specific UID: " + profile.uid);
     //         console.log("  Name: " + profile.displayName);
     //         console.log("  Email: " + profile.email);
     //         console.log("  Photo URL: " + profile.photoURL);
-    
+
     //         name = profile.displayName;
     //         profile = profile;
-//         console.log(profile);
+    //         console.log(profile);
 
-//       });
+    //       });
 
-//     }
+    //     }
 
-//     $('#greeting').text('Hey, ' + name + '!');
-//     $('#no-user').hide();
-//     $('#signed-in').show();
+    //     $('#greeting').text('Hey, ' + name + '!');
+    //     $('#no-user').hide();
+    //     $('#signed-in').show();
 
-//     $('#sign-out').on('click', function () {
+    //     $('#sign-out').on('click', function () {
 
-//       firebase.auth().signOut().then(function () {
+    //       firebase.auth().signOut().then(function () {
 
-//         $('#no-user').show();
-//         $('#signed-in').hide();
+    //         $('#no-user').show();
+    //         $('#signed-in').hide();
 
-//         console.log('signed out succesfully');
-//         // Sign-out successful.
-//       }).catch(function (error) {
-//         // An error happened.
-//         console.log('error signing out');
+    //         console.log('signed out succesfully');
+    //         // Sign-out successful.
+    //       }).catch(function (error) {
+    //         // An error happened.
+    //         console.log('error signing out');
 
-//       })
-//     })
-
-
-
-//     // User is signed in.
-//   } else {
-//     // No user is signed in.
-
-//     console.log('no user signed in');
-//   }
-// });
+    //       })
+    //     })
 
 
 
-// firebase.auth().onAuthStateChanged(function(user) {
-//   if (user) {
-//     // User is signed in.
-//     var displayName = user.displayName;
-//     var email = user.email;
-//     var emailVerified = user.emailVerified;
-//     var photoURL = user.photoURL;
-//     var isAnonymous = user.isAnonymous;
-//     var uid = user.uid;
-//     var providerData = user.providerData;
-//     // ...
-//   } else {
-//     // User is signed out.
-//     // ...
-//   }
-// });
+    //     // User is signed in.
+    //   } else {
+    //     // No user is signed in.
+
+    //     console.log('no user signed in');
+    //   }
+    // });
+
+
+
+    // firebase.auth().onAuthStateChanged(function(user) {
+    //   if (user) {
+    //     // User is signed in.
+    //     var displayName = user.displayName;
+    //     var email = user.email;
+    //     var emailVerified = user.emailVerified;
+    //     var photoURL = user.photoURL;
+    //     var isAnonymous = user.isAnonymous;
+    //     var uid = user.uid;
+    //     var providerData = user.providerData;
+    //     // ...
+    //   } else {
+    //     // User is signed out.
+    //     // ...
+    //   }
+    // });
 
 
 
 
-// CHAT
-function login() {
-  // Log the user in via Twitter
-  var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider).catch(function (error) {
-    console.log("Error authenticating user:", error);
-  });
-}
+    // CHAT
+    function login() {
+      // Log the user in via Twitter
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).catch(function (error) {
+        console.log("Error authenticating user:", error);
+      });
+    }
 
-firebase.auth().onAuthStateChanged(function (user) {
-  // Once authenticated, instantiate Firechat with the logged in user
-  if (user) {
-    initChat(user);
-  }
-});
+    firebase.auth().onAuthStateChanged(function (user) {
+      // Once authenticated, instantiate Firechat with the logged in user
+      if (user) {
+        initChat(user);
+      }
+    });
 
-function initChat(user) {
-  // Get a Firebase Database ref
-  var chatRef = firebase.database().ref("chat");
+    function initChat(user) {
+      // Get a Firebase Database ref
+      var chatRef = firebase.database().ref("chat");
 
-  // Create a Firechat instance
-  var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
+      // Create a Firechat instance
+      var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
 
-  // Set the Firechat user
-  chat.setUser(user.uid, user.displayName);
-}
+      // Set the Firechat user
+      chat.setUser(user.uid, user.displayName);
+    }
